d737db9c70c0356e82ebbf4793030493659998e3
Valerii <sepjani@gmail.com>
2024-12-30 12:00:45 +0000

    removed naughtyattributes dependency (1)

--- Scripts/Data/SOGroup.cs
+++ Scripts/Data/SOGroup.cs
@@ -1,12 +1,11 @@
 using System.Collections.Generic;
-using NaughtyAttributes;
 using UnityEngine;
 
 namespace UnityBlocks.Helpers.Data
 {
     public class SOGroup<T> : ScriptableObject where T : ScriptableObject
     {
-        [SerializeField, Expandable] private List<T> list;
+        [SerializeField] private List<T> list;
 
         public List<T> List => list;
     }
